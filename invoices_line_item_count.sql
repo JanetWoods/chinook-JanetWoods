8b7fcd633db13b7f807a7f8d282d5f2ab1fab797
Janet Woods <janetnwoods@gmail.com>
2019-05-21 20:13:41 -0500

    18 works with ids, no names. I WANT NAMES!

--- invoices_line_item_count.sql
+++ invoices_line_item_count.sql
@@ -1,2 +1,6 @@
 --17 Provide a query that shows all Invoices but includes the # of invoice line items.
 
+SELECT COUNT(L.InvoiceLineId) LineItems
+from Invoice i
+JOIN InvoiceLine L ON L.InvoiceId = I.InvoiceId
+GROUP by i.InvoiceId
--- sales_agent_total_sales.sql
+++ sales_agent_total_sales.sql
@@ -1,1 +1,6 @@
---  Provide a query that shows total sales made by each sales agent.
+-- 18 Provide a query that shows total sales made by each sales agent.
+SELECT e.EmployeeId,  sum(i.Total) Sales
+from Employee E
+JOIN Customer c on c.SupportRepId = e.EmployeeId
+join Invoice i on i.CustomerId = c.CustomerId
+GROUP by e.EmployeeId
